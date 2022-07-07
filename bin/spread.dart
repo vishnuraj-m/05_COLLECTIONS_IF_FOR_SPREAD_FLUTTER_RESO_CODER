2bb77fcb94d46052bed8091e982507382e75c00e
vishnuacappella <acappella.studiov@gmail.com>
2022-07-08 04:28:39 +0530

    spread operator used

--- bin/spread.dart
+++ bin/spread.dart
@@ -7,6 +7,14 @@ void main(List<String> arguments) {
 
   final x = <String>[
     for (int i = 0; i < 5; i++) i.toString(),
+    for (final number in [1, 2, 3]) number.toString(),
   ];
+  final list1 = ['hello', 'there'];
+  final list2 = ['What', 'up'];
   print(x);
+  final z = <String>[
+    ...list1,
+    ...list2,
+  ];
+  print(z);
 }
